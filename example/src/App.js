7dec7d82e52b53c55dbaf0fb1b55614afa017879
Victor Arnaud <victorhad@gmail.com>
2020-04-18 21:07:44 -0300

    Create constants

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
-import Utilities from 'vwapp-utilities';
+import Utilities, { Run, By, To } from 'vwapp-utilities';
 
 class App extends Component {
   render() {
     return (
       <Container>
         <h1>Ambiente de desenvolvimento.</h1>
-        <p><b>Classes</b>: {Utilities.method(Utilities.LIST_TO_STRING, ["class1", "class2"], "-")}</p>
-        <p><b>URL</b>: {Utilities.method(Utilities.MAKE_URL, "calculo numerico")}</p>
-        <p><b>Normalize CPF</b>: {Utilities.normalize(Utilities.CPF, "40572590075")}</p>
-        <p><b>Validate CPF</b>: {String(Utilities.validate(Utilities.CPF, "40572590075"))}</p>
+        <p><b>Classes</b>: {Utilities.method(Run.LIST_TO_STRING, ["class1", "class2"], "-")}</p>
+        <p><b>URL</b>: {Utilities.method(Run.MAKE_URL, "calculo numerico")}</p>
+        <p><b>Normalize CPF</b>: {Utilities.normalize(To.CPF, "40572590075")}</p>
+        <p><b>Validate CPF</b>: {String(Utilities.validate(By.CPF, "40572590075"))}</p>
       </Container>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,92 +5,98 @@ import {
   normalizeValue, normalizePhone, normalizeZipCode
 } from "./normalizers";
 
-export default class Utilities {
+export const To = {
+  DATE: "DATE",
+  TIME: "TIME",
+  DATETIME: "DATETIME",
+  CPF: "CPF",
+  CNPJ: "CNPJ",
+  MONEY: "MONEY",
+  PHONE: "PHONE",
+  ZIP_CODE: "ZIP_CODE",
+  NUMBER: "NUMBER"
+};
 
-  static IS_EMPTY = "IS_EMPTY";
-  static FORMAT_WITH_LEFT_ZERO = "FORMAT_WITH_LEFT_ZERO";
-  static REPLACE_ALL = "REPLACE_ALL";
-  static MAKE_URL = "MAKE_URL";
-  static TO_BOOLEAN = "TO_BOOLEAN";
-  static ONLY_NUMBERS = "ONLY_NUMBERS";
-  static LIST_TO_STRING = "LIST_TO_STRING";
+export const By = {
+  CPF: "CPF",
+  CNPJ: "CNPJ",
+  EMAIL: "EMAIL",
+  PHONE: "PHONE",
+  PHOTO: "PHOTO",
+  NUMBER: "NUMBER"
+};
+
+export const Run = {
+  IS_EMPTY: "IS_EMPTY",
+  FORMAT_WITH_LEFT_ZERO: "FORMAT_WITH_LEFT_ZERO",
+  REPLACE_ALL: "REPLACE_ALL",
+  MAKE_URL: "MAKE_URL",
+  TO_BOOLEAN: "TO_BOOLEAN",
+  ONLY_NUMBERS: "ONLY_NUMBERS",
+  LIST_TO_STRING: "LIST_TO_STRING"
+};
+
+export default class Utilities {
 
   static method(type) {
     switch (type) {
-      case this.IS_EMPTY:
+      case Run.IS_EMPTY:
         return isEmpty.apply(null, [...arguments].slice(1));
-      case this.FORMAT_WITH_LEFT_ZERO:
+      case Run.FORMAT_WITH_LEFT_ZERO:
         return formatWithLeftZero.apply(null, [...arguments].slice(1));
-      case this.REPLACE_ALL:
+      case Run.REPLACE_ALL:
         return replaceAll.apply(null, [...arguments].slice(1));
-      case this.MAKE_URL:
+      case Run.MAKE_URL:
         return makeURL.apply(null, [...arguments].slice(1));
-      case this.TO_BOOLEAN:
+      case Run.TO_BOOLEAN:
         return toBoolean.apply(null, [...arguments].slice(1));
-      case this.ONLY_NUMBERS:
+      case Run.ONLY_NUMBERS:
         return onlyNumbers.apply(null, [...arguments].slice(1));
-      case this.LIST_TO_STRING:
+      case Run.LIST_TO_STRING:
         return toString.apply(null, [...arguments].slice(1));
       default:
         return null;
     }
   }
 
-  static DATE = "DATE";
-  static TIME = "TIME";
-  static DATETIME = "DATETIME";
-  static CPF = "CPF";
-  static CNPJ = "CNPJ";
-  static MONEY = "MONEY";
-  static PHONE = "PHONE";
-  static ZIP_CODE = "ZIP_CODE";
-  static NUMBER = "NUMBER";
-
   static normalize(type, value) {
     switch (type) {
-      case this.DATE:
+      case To.DATE:
         return normalizeDate(value);
-      case this.TIME:
+      case To.TIME:
         return normalizeTime(value);
-      case this.DATETIME:
+      case To.DATETIME:
         return normalizeDatetime(value);
-      case this.CPF:
+      case To.CPF:
         return normalizeCPF(value);
-      case this.CNPJ:
+      case To.CNPJ:
         return normalizeCNPJ(value);
-      case this.MONEY:
+      case To.MONEY:
         return normalizeValue(value);
-      case this.PHONE:
+      case To.PHONE:
         return normalizePhone(value);
-      case this.ZIP_CODE:
+      case To.ZIP_CODE:
         return normalizeZipCode(value);
-      case this.NUMBER:
+      case To.NUMBER:
         return onlyNumbers(value);
       default:
         return null;
     }
   }
 
-  static CPF = "CPF";
-  static CNPJ = "CNPJ";
-  static EMAIL = "EMAIL";
-  static PHONE = "PHONE";
-  static PHOTO = "PHOTO";
-  static NUMBER = "NUMBER";
-
   static validate(type, value) {
     switch (type) {
-      case this.CPF:
+      case By.CPF:
         return validateCpf(value);
-      case this.CNPJ:
+      case By.CNPJ:
         return validateCnpj(value);
-      case this.EMAIL:
+      case By.EMAIL:
         return validateEmail(value);
-      case this.PHONE:
+      case By.PHONE:
         return validatePhone(value);
-      case this.PHOTO:
+      case By.PHOTO:
         return validatePhoto(value);
-      case this.NUMBER:
+      case By.NUMBER:
         return isNumber(value);
       default:
         return null;
